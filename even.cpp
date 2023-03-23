f823616ba9d7da44b424354806ceb21e656cf1ff
Shaikh-ibrahim <90645853+Shaikh-ibrahim@users.noreply.github.com>
2023-03-24 05:21:24 +0530

    Add files via upload

---
+++ even.cpp
@@ -0,0 +1,14 @@
+// Write a program to check weather number is even or odd
+#include <iostream>  
+using namespace std;  
+int main()  
+{  
+int num=10;
+if(num%2==0){
+    cout<<"Number Is Even !";
+}
+else{
+    cout<<"Number Is Odd !";
+}
+return 0;  
+}
